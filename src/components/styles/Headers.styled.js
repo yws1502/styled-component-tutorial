682d5e7da02035c5d8a264097e02ac6cd90e118e
Woosang <yws1502@naver.com>
2022-01-26 22:52:05 +0900

    💄 헤더 스타일 컴포넌트 생성

--- src/components/styles/Headers.styled.js
+++ src/components/styles/Headers.styled.js
@@ -4,3 +4,17 @@ export const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.colors.header};
   padding: 40px 0;
 `;
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 40px;
+`
+
+export const Logo = styled.img``
+
+export const Image = styled.img`
+  width: 375px;
+  margin-left: 40px;
+`
